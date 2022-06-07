de4bab4b3e5349a1413679401ff832126b4561bc
renang09 <renanxd125@gmail.com>
2022-06-07 20:05:10 -0300

    Segunda Mudança no código

--- NumeroPrimo.java
+++ NumeroPrimo.java
@@ -20,10 +20,11 @@ public class NumeroPrimo {
 		System.out.println("Esse número é divisível por: ");
 		for (int i = 1; i < numero; i++) {
 			if (numero % i == 0) {
-				System.out.print(i + ", ");
+				System.out.print(i + "," );
 			}
 
 		}
+		System.out.print(numero);
 	}
 
 }
