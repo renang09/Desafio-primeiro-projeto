d908b77ec61c2dd48f4485d514aa18961a93e13e
renang09 <renanxd125@gmail.com>
2022-06-07 19:46:57 -0300

    Primeira mudança no código

--- NumeroPrimo.java
+++ NumeroPrimo.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
 
 public class NumeroPrimo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Digite um número: ");
+		int numero = scan.nextInt();
+
+		System.out.println();
+		if (numero % 2 == 0 || numero % 3 == 0) {
+			System.out.println("Esse número não é primo");
+
+		} else {
+			System.out.println("Esse número é primo ");
+		}
+
+		System.out.println("Esse número é divisível por: ");
+		for (int i = 1; i < numero; i++) {
+			if (numero % i == 0) {
+				System.out.print(i + ", ");
+			}
+
+		}
 	}
 
 }
